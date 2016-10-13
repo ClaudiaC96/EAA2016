a7786850ef6a81235097bdebcd70a5fcfc2a87c5
John Burns <jzburns@gmail.com>
2016-10-13 11:16:39 +0100

    week 4 lab

---
+++ week4/diary.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read var 
+ 
+echo `date +%D-%R` $var >> diary.txt
+
+
+
---
+++ week4/pipe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#ps -ef prints a snapshot of every process
+#in system
+
+#grep mysql searches for the text mysql
+#where does the text come from?  from the ps -ef 
+
+#this command now searches all the running processes looking
+#for the mysql process
+
+ps -ef | grep mysql
